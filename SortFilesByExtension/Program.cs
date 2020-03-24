2649679e999c45900fbe0621b1b89f131e7bdd1d
Stan_Kudri <Stan_Kudri@DESKTOP-MFMFFL4>
2020-03-24 11:58:36 +0300

    Добавьте файлы проекта.

---
+++ SortFilesByExtension/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SortFilesByExtension
+{
+    class Program
+    {
+        //Для данной папки, по расширению, найти все файлы этого расширения и выдать отсортированный массив по размеру файлов.
+
+        static void Main(string[] args)
+        {
+            string path = $@"X:\Фильмы";
+            if (Directory.Exists(path))
+            {
+                var sortBy = new SortByExtension();
+                sortBy.Sort(path);
+                sortBy.OutputSortedFiles();
+            }
+            else
+                Console.WriteLine("Используется не папка !");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SortFilesByExtension/SortByExtension.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SortFilesByExtension
+{
+    class FileMetadata
+    {
+        public string Name { get; }
+        public long Length { get; }
+
+        public FileMetadata(string name, long length)
+        {
+            Name = name;
+            Length = length;
+        }
+    }
+
+    class SortByExtension
+    {
+        private string[] SortedNames { get; set; }
+
+        public void Sort(string path)
+        {
+            List<FileMetadata> fileMetadatas = new List<FileMetadata>();
+
+            foreach (var files in Directory.EnumerateFiles(path, "*.mkv"))
+            {
+                var fileInfo = new FileInfo(files);
+                fileMetadatas.Add(new FileMetadata(fileInfo.Name,fileInfo.Length));
+                Console.WriteLine($"{fileInfo.Name} => {fileInfo.Length}");
+            }
+
+            SortedNames = fileMetadatas.OrderBy(x => x.Length).Select(x => x.Name).ToArray();                                  
+        }
+
+        public void OutputSortedFiles()
+        {
+            Console.WriteLine("\nОтсортированные Элементы:");
+            foreach (var files in SortedNames)
+            {
+                Console.WriteLine($"{files}");
+            }
+        }
+
+    }
+}
